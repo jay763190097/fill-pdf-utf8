114ef9d564e736effe0638e1afe3fc4374567c63
jiang meng <jiangmang.1992@163.com>
2017-01-16 13:10:04 +0800

    use xfdf

--- index.js
+++ index.js
@@ -4,58 +4,42 @@
 'use strict';
 
 const fs  = require('fs'),
-    Iconv = require('iconv').Iconv,
-    iconv = new Iconv('UTF-8', 'UTF-16'),
-    path  = require('path'),
+    Xfdf = require('xfdf'),
     exec = require('child_process').exec;
 
 exports.generatePdf = function(data, templatePath, extendArgs, outputFile, callback) {
 
-    generatorFdf(data,'./data.fdf');
-    let normalized = normalizeArgs(extendArgs, callback);
-    extendArgs = normalized.args;
-    callback   = normalized.callback;
-    let processArgs = [templatePath, 'fill_form','./data.fdf', 'output', outputFile].concat(extendArgs);
-    let cmd = 'pdftk',option = {
-        encoding: 'utf8',
-        timeout: 100000,
-        maxBuffer: 200*1024,
-        killSignal: 'SIGTERM',
-        cwd: null,
-        env: null
-    };
-    for(let i = 0;i<processArgs.length;i++){
-        cmd += ' '+processArgs[i];
-    }
-    exec(cmd,option,function (error, stdout, stderr) {
-        if(typeof(callback) === "function"){
-            callback(error, stdout, stderr);
+    generatorXFdf(data,templatePath,function (err) {
+        if(err)
+            throw err;
+        let normalized = normalizeArgs(extendArgs, callback);
+        extendArgs = normalized.args;
+        callback   = normalized.callback;
+        let processArgs = [templatePath, 'fill_form','./temp.xfdf', 'output', outputFile].concat(extendArgs);
+        let cmd = 'pdftk',option = {
+            encoding: 'utf8',
+            timeout: 100000,
+            maxBuffer: 200*1024,
+            killSignal: 'SIGTERM',
+            cwd: null,
+            env: null
+        };
+        for(let i = 0;i<processArgs.length;i++){
+            cmd += ' '+processArgs[i];
         }
+        exec(cmd,option,function (error, stdout, stderr) {
+            if(typeof(callback) === "function"){
+                callback(error, stdout, stderr);
+            }
+        });
     });
 };
 
-function generatorFdf(data, fileName) {
 
-    let header, body, footer, dataKeys;
-
-    header = new Buffer('%FDF-1.2\n\u00e2\u00e3\u00cf\u00d3\n1 0 obj \n<<\n/FDF \n<<\n/Fields [\n');
-    footer = new Buffer(']\n>>\n>>\nendobj \ntrailer\n\n<<\n/Root 1 0 R\n>>\n%%EOF\n');
-
-    dataKeys = Object.keys(data);
-    body = new Buffer([]);
-    for(let i=0; i<dataKeys.length; i++) {
-        let name = dataKeys[i];
-        let value = data[name];
-        body = Buffer.concat([ body, new Buffer("<<\n") ]);
-        body = Buffer.concat([ body, new Buffer("/T (") ]);
-        body = Buffer.concat([ body, iconv.convert(name.toString()) ]);
-        body = Buffer.concat([ body, new Buffer(")\n") ]);
-        body = Buffer.concat([ body, new Buffer("/V (") ]);
-        body = Buffer.concat([ body, iconv.convert(value.toString()) ]);
-        body = Buffer.concat([ body, new Buffer(")\n") ]);
-        body = Buffer.concat([ body, new Buffer(">>\n") ]);
-    }
-    fs.writeFileSync(fileName, Buffer.concat([ header, body, footer ]));
+function generatorXFdf(data, templatePath,callback) {
+    let builder = new Xfdf({ pdf: templatePath});
+    builder.fromJSON(data);
+    builder.generateToFile('./temp.xfdf',callback);
 }
 
 
