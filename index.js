1157d49e564bb3003d9b28312673266c4b76ffe4
jay <jiangmang.1992@163.com>
2019-04-10 00:19:51 +0800

    update readme && fix file path problem

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ exports.generatePdf = function(data, templatePath, extendArgs, outputFile, callb
     generatorXFdf(data,templatePath,outputFile,function (err) {
         if(err)
             throw err;
-        let processArgs = [templatePath,'fontPath',path.dirname(__filename)+'\\lib\\simfang.ttf','fill_form','-', 'output','-','flatten','<'+outputFile.substring(0,outputFile.lastIndexOf('.'))+'.xfdf'+'>', outputFile];
+        let processArgs = [templatePath,'fontPath',path.dirname(__filename)+path.sep+'lib'+path.sep+'simfang.ttf','fill_form','-', 'output','-','flatten','<'+outputFile.substring(0,outputFile.lastIndexOf('.'))+'.xfdf'+'>', outputFile];
         if(typeof extendArgs === 'object') {
             for(let i in extendArgs) {
                 processArgs.push(i);
                 processArgs.push(extendArgs[i]);
             }
         }
-        let cmd = 'java -jar "'+path.dirname(__filename)+'\\lib\\fill_pdf_utf8.jar"',option = {
+        let cmd = 'java -jar "'+path.dirname(__filename)+path.sep+'lib'+path.sep+'fill_pdf_utf8.jar"',option = {
             encoding: 'utf8',
             timeout: 100000,
             maxBuffer: 200*1024,
